bc5c0ed2f39d89e01e9f8b0d432cceaeda573180
搬码道人 <735196070@qq.com>
2022-06-28 11:42:55 +0800

    update spring-boot start calss

--- src/main/java/com/bofangcp/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/bofangcp/springbootdemo/SpringbootDemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 //@ComponentScan(); --重新换地方
 public class SpringbootDemoApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
